6da1e4638c74772db59f9752df36673d162d8339
summsu84 <summsu84@gmail.com>
2020-08-05 15:25:07 +0900

    axios component add

---
+++ 3_vuex_test/src/components/axios/AxiosComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    Axios Test
+    <button @click="sendData">sendData</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'AxiosComponent',
+    methods: {
+      sendData () {
+        // this.$store.state.counter++;
+        console.log('first post');
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
